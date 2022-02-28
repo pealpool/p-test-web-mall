2a62eef6b96b4a27ac59b083fd56c8ca23b47278
pealpool <pealpool@gmail.com>
2022-02-28 17:17:38 +0800

    router

--- README.md
+++ README.md
@@ -8,10 +8,9 @@ http://39.98.123.211:8170/swagger-ui.html
 http://39.98.123.211:8216/swagger-ui.html
 
 * 代码：
-
-    https://gitee.com/jch1011/shangpinhui_0415
-https://gitee.com/jch1011/guigu
-https://pan.baidu.com/s/1BM_OKMXXAGxMNqaBN_7tRg#list
+* https://gitee.com/jch1011/shangpinhui_0415
+* https://gitee.com/jch1011/guigu
+* https://pan.baidu.com/s/1BM_OKMXXAGxMNqaBN_7tRg#list
 
 * 笔记
 https://blog.csdn.net/weixin_43424325/article/details/121684101
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+Vue.use(VueRouter)
+import Home from "@/pages/Home";
+import Search from "@/pages/Search";
+import Detail from "@/pages/Detail";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            name:'search',
+            path: '/search/:id',
+            component: Search
+        },
+        {
+            path: '/detail',
+            component: Detail
+        },
+        {
+            path: '/login',
+            component: Login,
+            meta:{
+                footerHide:true
+            }
+        },
+        {
+            path: '/register',
+            component: Register,
+            meta:{
+                footerHide:true
+            }
+        },
+
+    ]
+})
